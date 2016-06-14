860572f0922c1d71a9061ab7b03c79051790e7b5
kateah1 <kate.hurd64@gmail.com>
2016-06-14 11:18:20 -0400

    1118

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 $(document).ready(function() {
 
 
-	var integer = Number(document.getElementById("#item"))
+	function fizzbuzz(integer) {
+		
+		if(isNaN(integer)) {
+			alert("Please enter a valid number!");
+		}
+		else {
 
-	$("#submit").on('click', function(){
+			for (var num = 1; num <= integer; num++) {
 
-		for (var num = 1; num <= integer; num++) {
+				if (num % 3 === 0 && num % 5 === 0) {
+					$("ul").append("<li>" + "FizzBuzz" + "</li>");
+				}
+				else if(num % 3 === 0) {
+					$("ul").append("<li>" + "Fizz" + "</li>");
+				}
+				else if(num % 5 === 0) {
+					$("ul").append("<li>" + "Buzz" + "</li>");
+				}
+				else {
+					$("ul").append("<li>" + num + "</li>");
+				}	
+			}
+		}		
+	}	
 
-			if (integer % 3 == 0 && integer % 5 == 0) {
-				$("ul").append("<li>" + "FizzBuzz" + "</li>");
-			}
-			else if(integer % 3 == 0) {
-				$("ul").append("<li>" + "Fizz" + "</li>");
-			}
-			else if(integer % 5 == 0) {
-				$("ul").append("<li>" + "Buzz" + "</li>");
-			}
-			else {
-				$("ul").append("<li>" + integer + "</li>");
-			};	
-		};	
-	});
+	$("button").on('click', function() {
+		$("ul").empty();
+		fizzbuzz(Number($("#item").val()));
+	})
 
 });
